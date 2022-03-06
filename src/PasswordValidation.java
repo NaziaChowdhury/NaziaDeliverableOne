bf88a443cb558735d901e7de859eef0e608f38c9
nazia_chowdhury <nituchowdhury96@gmail.com>
2022-03-06 15:16:24 -0500

    ask user to create password

--- src/PasswordValidation.java
+++ src/PasswordValidation.java
@@ -1,8 +1,17 @@
 
 public class PasswordValidation {
 
+	
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hello! Please create a password. The password must contain:\n"
+				+ "○ At least one lowercase letter\n"
+				+ "○ At least one uppercase letter\n"
+				+ "○ At least minimum 7 characters\n"
+				+ "○ At least maximum 12 characters\n"
+				+ "○ An exclamation point: !");
+			
 
 	}
 
