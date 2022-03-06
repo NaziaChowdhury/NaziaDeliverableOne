5021f7dcc4f6962d6c56facaa52a445d29853a0f
nazia_chowdhury <nituchowdhury96@gmail.com>
2022-03-06 15:46:18 -0500

    check for maximum length

--- src/PasswordValidation.java
+++ src/PasswordValidation.java
@@ -2,28 +2,21 @@ import java.util.Scanner;
 
 public class PasswordValidation {
 
-	
-
 	public static void main(String[] args) {
-		
-		System.out.println("Hello! Please create a password. The password must contain:\n"
-				+ "○ At least one lowercase letter\n"
-				+ "○ At least one uppercase letter\n"
-				+ "○ At least minimum 7 characters\n"
-				+ "○ At least maximum 12 characters\n"
-				+ "○ An exclamation point: !");
-		
+
+		System.out.println(
+				"Hello! Please create a password. The password must contain:\n" + "○ At least one lowercase letter\n"
+						+ "○ At least one uppercase letter\n" + "○ At least minimum 7 characters\n"
+						+ "○ At least maximum 12 characters\n" + "○ An exclamation point: !");
+
 		Scanner scanner = new Scanner(System.in);
 		String password = scanner.nextLine();
-		
-		if (password.length() >= 7 ) {
+
+		if (password.length() >= 7 && password.length() <= 12) {
 			System.out.println("Password valid and accepted");
 		} else {
 			System.out.println("Error");
 		}
-		
-		
-			
 
 	}
 
